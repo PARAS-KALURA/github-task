c9b5dd2da425ea8b26b7ca343055d0037cc1f8eb
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-08 11:14:18 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,193 +1,24 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useFormStatus } from 'react-dom'
 
 const App = () => {
+  
+  const [count, setCount] = useState(0);
 
-  const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState("");
-
-  const handleAddTask = () => {
-    const trimmed = input.trim();
-
-    if (!trimmed) return;
-
-    setTasks([...tasks, trimmed]);
-    setInput('');
-
-
-  }
-
-  const handleDeleteTask = (index) => {
-  const updatedTasks = [...tasks];
-  updatedTasks.splice(index,1);
-  setTasks(updatedTasks);    
-  };
-
+  useEffect(() => {
+     document.title = `Your Count Count: ${count}`;
+  },[count])
 
   return (
-    <div
+    <div>
+      <h1
       style={{
-        width: "70%",
-        minWidth: "300px",
-        maxWidth: "800px",
-        margin: "0 auto",
-        marginTop: "50px",
+        color: "white",
       }}
-    >
-      <div
-        style={{
-          backgroundColor: "#161B21",
-          margin: "0 auto",
-          marginTop: "50px",
-          padding: "20px",
-          border: "1px solid gray",
-          width: "100%",
-          maxWidth: "800px",
-          textAlign: "left",
-          minHeight: "200px",
-          borderRadius: "4px",
-          fontFamily:
-          "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif",
-        }}
-      >
-        <div
-          style={{
-            padding: "14px 16px",
-            borderBottom: "1px solid #30363d",
-            fontWeight: "600",
-            display: "flex",
-            alignItems: "center",
-            gap: "8px",
-          }}
-        >
-          <div
-            style={{
-              color: "white",
-              fontSize: "22px",
-            }}
-          >
-            <span>❗</span>
-            <span>GitHub's Theme</span>
-          </div>
-
-        </div>
-
-        <div>
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleAddTask();
-              }
-            }}
-
-            style={{
-              color: "white",
-              padding: "12px",
-              backgroundColor: "black",
-              margin: "6px",
-              border: "none",
-              width: "90%",
-              borderRadius: "7px",
-              marginRight: "8px",
-            }}
-            type="text"
-            placeholder="Create a new task..."
-          />
-
-
-          <button
-            onClick={handleAddTask}
-            style={{
-              backgroundColor: "green",
-              border: "none",
-              color: "white",
-              padding: '9px 6px',
-              borderRadius: "4px",
-              gap: "4px",
-              cursor: "pointer",
-
-            }}
-          >Add</button>
-
-        </div>
-
-        <hr style={{ border: "1px solid #30363d", marginTop: "10px" }} />
-
-
-        {tasks.length === 0 ? (<p
-          style={{
-            textAlign: "center",
-            color: "#6e7681",
-            padding: "31px 0",
-          }}
-        >
-          No tasks yet — create one above.
-        </p>
-        ) : (
-          <ul
-            style={{
-              listStyle: "none",
-              gap: "10px",
-              display: "flex",
-              flexDirection: "column",
-              marginTop: "16px",
-            }}
-          >
-            {tasks.map((task, index) => {
-              return <li
-                style={{
-                  transition: "background 0.2s",
-                  border: "1px solid #30363d",
-                  padding: "6px",
-                  borderRadius: "6px",
-                  cursor: "pointer",
-                  color: "white",
-                  backgroundColor: "#0d1117",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-                onMouseEnter={(e) => (e.target.style.background = "#1d2633")}
-                onMouseLeave={(e) => (e.target.style.background = "#0d1117")}
-
-
-
-
-                key={index} >{task}
-
-                <button
-                onClick={() => handleDeleteTask(index)}
-                  style={{
-                    backgroundColor: 'red',
-                    color: "white",
-                    border: "none",
-                    padding: "4px",
-                    borderRadius: "3px",
-                    cursor: "pointer",
-
-                  }}
-
-
-
-
-                >Delete</button>
-
-
-
-
-              </li>
-            })}
-          </ul>
-
-        )}
-
-
-
-
-      </div>
+      >Count:{count}</h1>
+      <button
+      onClick={() => setCount(count+1)}
+      >Increase</button>
     </div>
   )
 }
