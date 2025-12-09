930749c66604a413e433b1104ce6c967d0ea3ee2
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-09 14:48:15 +0530

    doing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
- 
-  const[text,setText] = useState("");
 
-   useEffect(() => {
-    const saved = localStorage.getItem("savedItem");
-    if(saved) {
-      setText(saved);
-    }
-  },[]);
+   const[imageUrl, setImageUrl] = useState("");
 
-    useEffect(() => {
-    localStorage.setItem("savedItem", text);
-    
-  },[text]);
-
-  useEffect(() => {
-     document.title = `Text: ${text}`
-  },[text]);
-
+   const generateImage = async() => {
+    const response = await fetch("https://picsum.photos/400");
+    setImageUrl(response.url);
+   }
 
   return (
-    <div>
-      <input type="text"
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-      />
-      <p>You typed: {text}</p>
+    <div
+    style={{
+      height: "100vh",
+      width: "100vw",
+      display: "flex",
+      marginTop: "10px",
+      alignItems: "center",
+      color: "white",
+      gap: "10px",
+      flexDirection: "column",
+    }}
+    >
+     <h1
+     >Random Image Generator</h1>
+     <button
+     onClick={generateImage}
+     style={{
+      padding: "7px",
+      borderRadius: "5px",
+      border: "none",
+     }}
+     >Generate Image</button>
+
+     <img src="{imageUrl}" alt="img" />
+
     </div>
   )
 }
