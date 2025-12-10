eaa588c11a1f502a75a476c539a6f9d2de3b2bd3
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-10 23:07:46 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const[city, setCity] = useState("");
+  const[loading, setLoading] = useState(null);
+  const[weather, setWeather] = useState(false);
+  const[error, setError] = useState("");
+
+    const API_KEY = "YOUR_API_KEY_HERE";
+
+    async function getWeather() {
+     
+      if(!city) {
+        setError("Please enter a city name");
+        return;
+      }
+
+      setLoading(true);
+      setError("");
+      setWeather(null);
+
+    }
+
+  function handleClick(e) {
+    setCity(e.target.value);
+  }
+
+  
+
   return (
     <>
       <h2
@@ -27,6 +54,7 @@ const App = () => {
         }}
       >
         <input
+        onChange={handleClick}
           placeholder="Search City..."
           style={{
             width: "80%",
@@ -40,6 +68,14 @@ const App = () => {
           }}
           type="text"
         />
+
+       <p
+       style={{
+        color: "white",
+        marginTop: "20px",
+       }}
+       >You Typed: {city}</p>
+
       </div>
     </>
   );
