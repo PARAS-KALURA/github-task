43eb5815a4acc5ce844a3274bacfe80c4537b15d
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-11 14:38:48 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,62 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const App = () => {
 
   const[city, setCity] = useState("");
-  const[loading, setLoading] = useState(null);
-  const[weather, setWeather] = useState(false);
-  const[error, setError] = useState("");
 
-    const API_KEY = "YOUR_API_KEY_HERE";
-
-    async function getWeather() {
-     
-      if(!city) {
-        setError("Please enter a city name");
-        return;
-      }
-
-      setLoading(true);
-      setError("");
-      setWeather(null);
-
-    }
-
-  function handleClick(e) {
-    setCity(e.target.value);
-  }
-
-  
+ 
 
   return (
-    <>
-      <h2
-        style={{
-          color: "white",
-          textAlign: "center",
-          fontSize: "40px",
-          marginBottom: "20px",
-        }}
-      >
-        Weather App
-      </h2>
-
+    <div >
       <div
-        style={{
-          borderRadius: "10px",
-          textAlign: "center",
-          border: "2px solid gray",
-          padding: "20px",
-          width: "90vw",
-          maxWidth: "400px",
-          margin: "0 auto",
-          backgroundColor: "#2a2a2a",
-        }}
+      style={{
+        border: "2px solid blue",
+        padding: "10px",
+        borderRadius: "20px",
+        textAlign: "center",
+         maxHeight: "400px", maxWidth: "600px",
+      }}
       >
-        <input
-        onChange={handleClick}
-          placeholder="Search City..."
-          style={{
-            width: "80%",
-            padding: "12px",
-            borderRadius: "10px",
-            color: "black",
-            backgroundColor: "white",
-            fontSize: "18px",
-            border: "none",
-            outline: "none",
-          }}
-          type="text"
-        />
+           <h2
+           style={{color: "white", }}
+           >Weather App</h2>
 
-       <p
-       style={{
-        color: "white",
-        marginTop: "20px",
-       }}
-       >You Typed: {city}</p>
+           <input
+           style={{
+            borderRadius: "4px",
+            border: "none",
+            padding: "16px",
+           }}
+           onChange={setCity}
+           type="text" placeholder='Search City' />
+
+           <button
+           style={{
+            marginLeft: "4px",
+            border: "none",
+            padding: "13px",
+            borderRadius: "4px",
+           }}
+           >👉</button>
+
+           <p
+           style={{
+            textAlign: "center",
+            border: "2px solid blue",
+            borderRadius: "5px",
+            minWidth: "200px",
+            padding: "5px",
+
+           }}
+           >
+            
+            City: {city}</p>
+
 
       </div>
-    </>
-  );
-};
 
-export default App;
+    </div>
+  )
+}
+
+export default App
