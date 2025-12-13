00a7b9a6397e54d990938fc15ba38ad511819319
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-13 21:09:35 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+
 
 const App = () => {
 
-  const[city, setCity] = useState("");
+ const[number, setNumber] = useState(0);
 
- 
+
+ const renderCount = useRef(1);
+
+
+
+function handleClick() {
+  setNumber(n => n + 1);
+  
+} 
+
+ useEffect(() => {
+   console.log("Renrenders");
+
+  renderCount.current = renderCount.current + 1;
+
+  console.log("Render Count:", renderCount.current);   
+ })
+
 
   return (
-    <div >
-      <div
-      style={{
-        border: "2px solid blue",
-        padding: "10px",
-        borderRadius: "20px",
-        textAlign: "center",
-         maxHeight: "400px", maxWidth: "600px",
-      }}
-      >
-           <h2
-           style={{color: "white", }}
-           >Weather App</h2>
-
-           <input
-           style={{
-            borderRadius: "4px",
-            border: "none",
-            padding: "16px",
-           }}
-           onChange={setCity}
-           type="text" placeholder='Search City' />
-
-           <button
-           style={{
-            marginLeft: "4px",
-            border: "none",
-            padding: "13px",
-            borderRadius: "4px",
-           }}
-           >👉</button>
-
-           <p
-           style={{
-            textAlign: "center",
-            border: "2px solid blue",
-            borderRadius: "5px",
-            minWidth: "200px",
-            padding: "5px",
-
-           }}
-           >
-            
-            City: {city}</p>
-
-
-      </div>
+    <div>
+      <button
+      onClick={handleClick}
+      >Click me!</button>
+     
+     <p>{number}</p>
+     <p>Component Rendered: {renderCount.current} times</p>
 
     </div>
   )
